ac09a762af3285ebbf81f1b5ba9e39f5c9e5529b
terryjung0244 <terryjung0244@gmail.com>
2023-06-10 22:17:14 -0400

    Created prettier/eslint

---
+++ src/service/components/create/CreateTodo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CreateTodo.css";
+
+const CreateTodo = () => {
+  return <div></div>;
+};
+
+export default CreateTodo;
--- src/service/store/index.ts
+++ src/service/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, useDispatch } from "react-redux";
+import todoReducer from "service/redux/reducer/todoReducer";
 
 export const createStore = () =>
   configureStore({
-    reducer: {},
+    reducer: {
+      todoReducer,
+    },
   });
 
 export const store = createStore();
